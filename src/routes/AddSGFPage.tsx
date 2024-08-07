d57f0b7e2d2b67202d4c43e8532bb81c736cdb42
Tomo Krajina <tkrajina@gmail.com>
2024-08-07 18:08:11 +0200

    Search games

--- src/routes/AddSGFPage.tsx
+++ src/routes/AddSGFPage.tsx
@@ -4,8 +4,13 @@ import { TargetedEvent } from 'preact/compat';
 import { useCallback } from 'preact/hooks';
 import { gamesService } from '../services/GamesService';
 import { BaseScreen } from '../components/BaseScreen';
+import { API_SERVICE } from '../services/APIService';
+import { API_Game } from '../models/models_generated';
+import { useStateRef } from '../utils/hooks';
 
 const AddSGF = () => {
+	const games = useStateRef<API_Game[] | undefined>(undefined)
+	const searchString = useStateRef<string>("")
 
 	const uploadFile = useCallback(async (event: TargetedEvent<HTMLInputElement>) => {
 		event.preventDefault();
@@ -26,15 +31,29 @@ const AddSGF = () => {
 		} catch (e) {
 			alert("Error importing: " + e);
 		}
-    }, []);
+    }, [])
 
+	const search = async () => {
+		try {
+			games.set(await API_SERVICE.doGET("/games", {"search": searchString.get()}) as API_Game[]);
+		} catch (e) {
+			alert(e);
+		}
+	}
 
 	return (
 		<BaseScreen selected='new'>
-			<h1>Add new game</h1>
+			<h2>Add new game</h2>
 			<p>
 				Select SGF file: <input type="file" name="myFile" onChange={uploadFile} accept=".sgf" />
 			</p>
+			<hr />
+			<h2>...or import from our collection</h2>
+			<input type="text" placeholder="Search" value={searchString.get()} onChange={e => searchString.set(e.currentTarget.value)}/>
+			<button onClick={search}>Search</button>
+			<ul>
+				{games.get() && games.get().map(game => <li>{JSON.stringify(game)}</li>)}
+			</ul>
 		</BaseScreen>
 	);
 };
