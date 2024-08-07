00e3d48ab00236a4e0bd10051c294b1f207d7976
Tomo Krajina <tkrajina@gmail.com>
2024-08-07 18:06:52 +0200

    Remove moves count hardcode

--- src/components/GameDesc.tsx
+++ src/components/GameDesc.tsx
@@ -9,6 +9,5 @@ export function GameDesc(props: {game: Game, event?: boolean, result?: boolean, 
     {(props.event && props.game.event) && <small><br/>Event: <strong>{props.game.event}</strong></small>}
     {(props.result && props.game.result) && <small><br/>Result: <strong>{props.game.result}</strong></small>}
     {(props.date && props.game.date) && <small><br/>Date: <strong>{props.game.date}</strong></small>}
-    {JSON.stringify(props.game?.movesCount)}
   </Fragment>
 }
